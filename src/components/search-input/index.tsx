f8d3f5112d9b4c2cbcd306171abe16aaae9ccac0
zhaolifei <zhaolifei@jd.com>
2021-03-24 17:35:55 +0800

    feat: 搜索组件丰富

--- src/components/search-input/index.tsx
+++ src/components/search-input/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{Component} from 'react';
 import css from './index.css';
 
 interface SearchInputProps {
@@ -6,21 +6,65 @@ interface SearchInputProps {
   placeholder:string
 }
 
-const SearchInput: React.FC<SearchInputProps> = ({
-  isHasBtn,
-  placeholder
-}) => {
-  return (
-    <div className={css.search_container}>
-      <div className={css.search_input_wrap}>
-        <div className={css.search_input_icon}></div>
-        <input type='text' placeholder={placeholder || '请输入搜索关键字'} className={[css.search_input,isHasBtn?css.has_btn:''].join(' ')} />
-        {isHasBtn && 
-          <div className={css.search_btn}>搜索</div>
-        }    
-      </div>
-    </div>
-  );
-};
+interface SearchInputState {
+  isShowClear: boolean,
+  keyWord: string
+}
 
-export default SearchInput
+export default class SearchInput extends Component<SearchInputProps,SearchInputState> {
+
+  constructor(props:any){
+    super(props)
+    this.state={
+      isShowClear:false,
+      keyWord:''
+    }
+  }
+
+  changeFunc = (e:any) => {
+    console.log("e=",e.target.value)
+    const keyWord = e.target.value
+    let _isShowClear = false
+    if(keyWord){
+      _isShowClear = true
+    }
+    this.setState({
+      isShowClear: _isShowClear,
+      keyWord
+    })
+  }
+
+  clearKeyWord=()=>{
+    this.setState({
+      isShowClear: false,
+      keyWord:''
+    })
+  }
+
+  gotoSearch=()=>{
+    alert("进入搜索页面")
+  }
+
+  render(){
+    const {isHasBtn,placeholder} = this.props
+    const {isShowClear,keyWord} = this.state
+    return (
+      <div className={css.search_container}>
+        <div className={css.search_input_wrap}>
+          <div className={css.search_input_icon}></div>
+          {isShowClear && <div className={[css.clear_input_icon,isHasBtn?css.has_btn:''].join(' ')} onClick={()=>{
+            this.clearKeyWord()
+          }}></div>}
+          <input type='text' placeholder={placeholder || '请输入搜索关键字'} onChange={(e)=>{
+            this.changeFunc(e)
+          }} className={[css.search_input,isHasBtn?css.has_btn:''].join(' ')} value={keyWord} />
+          {isHasBtn && 
+            <div className={css.search_btn} onClick={()=>{
+              this.gotoSearch()
+            }}>搜索</div>
+          }    
+        </div>
+      </div>
+    );
+  }
+}
