af1632381c181f33eda6a6e730e27d73cc0d7c9e
yanghongmei1 <yanghongmei1@jd.com>
2021-06-29 15:28:40 +0800

    去掉组件重新编译

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM base_image
-ADD . /home/
-WORKDIR /home
-ENTRYPOINT /usr/sbin/sshd && mkdir -p /export/logs && mkdir -p /dev/shm/nginx_temp/client_body && nginx -c /opt/nginx/conf/nginx.conf  && sleep 9999999d
-
-# && NODE_ENV=production pm2 startOrRestart /home/bin/start.json && sleep 9999999d
--- src/components/drawer-icons/index.css.d.ts
+++
@@ -1,8 +0,0 @@
-export const container: string;
-export const container__wrapper: string;
-export const container__wrapper__inner: string;
-export const container__wrapper__inner__icon: string;
-export const container__wrapper__inner__icon__0:string;
-export const container__wrapper__inner__icon__1: string;
-export const container__wrapper__inner__icon__2: string;
-export const container__wrapper__inner__icon__close: string;
